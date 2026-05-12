9496c276247825e9e97e992bddb21bbb559382c5
Ingve Værnes <ingve09@gmail.com>
2026-05-12 21:17:17 +0200

    fix: made logic/target become ignored

--- logic/target/debug/deps/logic-2fe780064fe792d5.d
+++
@@ -1,5 +0,0 @@
-/Users/ingvearnes/Documents/idatt2104_frivillig_prosjekt/logic/target/debug/deps/logic-2fe780064fe792d5.d: src/main.rs
-
-/Users/ingvearnes/Documents/idatt2104_frivillig_prosjekt/logic/target/debug/deps/liblogic-2fe780064fe792d5.rmeta: src/main.rs
-
-src/main.rs:
